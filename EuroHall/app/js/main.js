976b6797b58977c43ffa048bffc3c35719d81b1b
Slavik <saslavik@gmail.com>
2021-05-04 23:46:16 +0300

    first commit

---
+++ EuroHall/app/js/main.js
@@ -0,0 +1,162 @@
+// const body = document.body;
+// const menu = document.querySelector(".menu");
+// const menuBtn = document.querySelector(".menu-btn");
+// const player = new Plyr('#player', {
+//   fullscreen: {
+//     iosNative: true,
+//     container: null
+//   },
+// });
+
+
+// menuBtn.addEventListener("click", function () {
+//   menuBtn.classList.toggle("active");
+//   menu.classList.toggle("active");
+//   body.classList.toggle("active");
+// });
+
+// $(".menu__link").on("click", function () {
+//   menuBtn.classList.remove("active");
+//   menu.classList.remove("active");
+//   body.classList.remove("active");
+// });
+
+// $(document).ready(function() {
+//   setInterval('blockAnimate();', 4000);
+// });
+
+// // tabs unchecked
+// const allRadios = document.getElementsByName("tabs2");
+// var radioChecked;
+// for (let i = 0; i < allRadios.length; i++) {
+//   allRadios[i].onclick = function () {
+//     if (radioChecked == this) {
+//       this.checked = false;
+//       radioChecked = null;
+//     } else {
+//       radioChecked = this;
+//     }
+//   };
+// }
+
+// function toggleHeader() {
+//   var scroll_status = $(document).scrollTop();
+//   if (scroll_status > 300) $(".header__fixing").addClass("header__fixed");
+//   else $(".header__fixing").removeClass("header__fixed");
+// }
+// toggleHeader();
+// $(document).scroll(function () {
+//   toggleHeader();
+// });
+
+// //swiper
+
+// $(document).ready(function(){
+//   $('.reviews__slider .swiper-wrapper').slick({
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     swipeToSlide: true,
+//     touchThreshold: 15,
+//     speed: 150,
+//     responsive: [
+//       {
+//         breakpoint: 762,
+//         settings: {
+//           slidesToShow: 2
+//         }
+//       },
+//       {
+//         breakpoint: 486,
+//         settings: {
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+//   $('.swiper-gallery .swiper-wrapper').slick({
+//     infinite: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     swipeToSlide: true,
+//   });
+//   $('.swiper-mobile .swiper-wrapper').slick({
+//     infinite: true,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     swipeToSlide: true,
+//     adaptiveHeight: true,
+//     centerMode: true,
+//     variableWidth: true,
+//     speed: 150,
+//     responsive: [
+//       {
+//         breakpoint: 486,
+//         settings: {
+//           centerMode: false,
+//           variableWidth: false,
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+// });
+
+// $("a.menu__link")
+//   .add("a.page__link")
+//   .on("click", function (event) {
+//     event.preventDefault();
+//     var id = $(this).attr("href"),
+//       top = $(id).offset().top;
+//       if (id === '#contact') {
+//         top -= 150;
+//       }
+//     $("body,html").animate({ scrollTop: top }, 1500, "linear");
+//   });
+
+//   $('.gallery').on('selectstart', function (event) {
+//     event.preventDefault();
+//   })
+
+// const swiperGallery = new Swiper(".swiper-gallery", {
+//   slidesPerView: 1,
+//   slidesPerColumn: 1,
+//   spaceBetween: 0,
+//   slidesOffsetBefore: 0,
+//   loop: true,
+//   freeMode: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".gallery-button-next",
+//     prevEl: ".gallery-button-prev",
+//   },
+// });
+// const swiperMobile = new Swiper(".swiper-mobile", {
+//   slidesPerView: "auto",
+//   spaceBetween: 1,
+//   autoHeight: true,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".gallery-button-next",
+//     prevEl: ".gallery-button-prev",
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       freeMode: false,
+//     },
+//     510: {
+//       slidesPerView: 2,
+//       freeMode: false,
+//     },
+//   },
+// });
+
+//# sourceMappingURL=main.js.map
---
+++ EuroHall/gulpfile.js
@@ -0,0 +1,184 @@
+const {src, dest, parallel, series, watch} = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify-es').default;
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const svgSprite = require('gulp-svg-sprite');
+const fileInclude = require('gulp-file-include');
+const sourcemaps = require('gulp-sourcemaps');
+const rev = require('gulp-rev');
+const revRewrite = require('gulp-rev-rewrite');
+const revDel = require('gulp-rev-delete-original');
+const htmlmin = require('gulp-htmlmin');
+const gulpif = require('gulp-if');
+const notify = require('gulp-notify');
+const image = require('gulp-image');
+const { readFileSync } = require('fs');
+const concat = require('gulp-concat');
+
+let isProd = false; // dev by default
+
+const clean = () => {
+	return del(['app/*'])
+}
+
+//svg sprite
+const svgSprites = () => {
+  return src('./src/img/svg/**.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: "../sprite.svg" //sprite file name
+        }
+      },
+    }))
+    .pipe(dest('./app/img'));
+}
+
+const styles = () => {
+  return src(['./src/scss/**/*.scss', './node_modules/swiper/swiper-bundle.css'])
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(sass().on("error", notify.onError()))
+    .pipe(autoprefixer({
+      cascade: false,
+    }))
+    .pipe(gulpif(isProd, cleanCSS({ level: 2 })))
+    // .pipe(gulpif(!isProd, sourcemaps.write('.')))
+    .pipe(dest('./app/css/'))
+    .pipe(browserSync.stream());
+};
+
+const stylesBackend = () => {
+	return src(['./src/scss/**/*.scss', './node_modules/swiper/swiper-bundle.css'])
+		.pipe(sass().on("error", notify.onError()))
+    .pipe(autoprefixer({
+      cascade: false,
+		}))
+		.pipe(dest('./app/css/'))
+};
+
+const scripts = () => {
+	src('./src/js/vendor/**.js')
+		.pipe(concat('vendor.js'))
+		.pipe(gulpif(isProd, uglify().on("error", notify.onError())))
+		.pipe(dest('./app/js/'))
+  return src(
+    ['./src/js/functions/**.js', './src/js/components/**.js', './src/js/main.js'])
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(concat('main.js'))
+    .pipe(gulpif(isProd, uglify().on("error", notify.onError())))
+    .pipe(gulpif(!isProd, sourcemaps.write('.')))
+    .pipe(dest('./app/js'))
+    .pipe(browserSync.stream());
+}
+
+const scriptsBackend = () => {
+	src('./src/js/vendor/**.js')
+    .pipe(concat('vendor.js'))
+    .pipe(gulpif(isProd, uglify().on("error", notify.onError())))
+		.pipe(dest('./app/js/'))
+	return src(['./src/js/functions/**.js', './src/js/components/**.js', './src/js/main.js'])
+    .pipe(dest('./app/js'))
+};
+
+const resources = () => {
+  return src('./src/resources/**')
+    .pipe(dest('./app'))
+}
+
+const images = () => {
+  return src([
+		'./src/img/**.jpg',
+		'./src/img/**.png',
+		'./src/img/**.jpeg',
+		'./src/img/**.svg',
+		'./src/img/**.mp4',
+		'./src/img/**/*.jpg',
+		'./src/img/**/*.png',
+		'./src/img/**/*.jpeg',
+		'./src/img/**/*.svg',
+		'./src/img/**/*.mp4'
+		])
+    .pipe(gulpif(isProd, image()))
+    .pipe(dest('./app/img'))
+};
+
+const imagesSvg = () => {
+  return src([
+		'./src/img/**.svg',
+		'./src/img/**/*.svg',
+		])
+    .pipe(dest('./app/img'))
+};
+
+const htmlInclude = () => {
+  return src(['./src/*.html'])
+    .pipe(fileInclude({
+      prefix: '@',
+      basepath: '@file'
+    }))
+    .pipe(dest('./app'))
+    .pipe(browserSync.stream());
+}
+
+const watchFiles = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./app"
+    },
+  });
+
+  watch('./src/scss/**/*.scss', styles);
+  watch('./src/js/**/*.js', scripts);
+  watch('./src/partials/*.html', htmlInclude);
+  watch('./src/*.html', htmlInclude);
+  watch('./src/resources/**', resources);
+  watch('./src/img/*.{jpg,jpeg,png,svg,mp4}', images);
+	watch('./src/img/**/*.{jpg,jpeg,png,svg,mp4}', images);
+	watch('./src/img/*.{jpg,jpeg,png,svg,mp4}', imagesSvg);
+	watch('./src/img/**/*.{jpg,jpeg,png,svg,mp4}', imagesSvg);
+  watch('./src/img/svg/**.svg', svgSprites);
+}
+
+const cache = () => {
+  return src('app/**/*.{css,js,svg,png,jpg,jpeg,woff2}', {
+    base: 'app'})
+    .pipe(rev())
+    .pipe(dest('app'))
+    .pipe(revDel())
+    .pipe(rev.manifest('rev.json'))
+    .pipe(dest('app'));
+};
+
+const rewrite = () => {
+  const manifest = readFileSync('app/rev.json');
+
+  return src('app/**/*.html')
+    .pipe(revRewrite({
+      manifest
+    }))
+    .pipe(dest('app'));
+}
+
+const htmlMinify = () => {
+	return src('app/**/*.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
+		.pipe(dest('app'));
+}
+
+const toProd = (done) => {
+  isProd = true;
+  done();
+};
+
+exports.default = series(clean, htmlInclude, scripts, styles, resources, images, imagesSvg, svgSprites, watchFiles);
+
+exports.build = series(toProd, clean, htmlInclude, scripts, styles, resources, images, imagesSvg, svgSprites, htmlMinify);
+
+exports.cache = series(cache, rewrite);
+
+exports.backend = series(toProd, clean, htmlInclude, scriptsBackend, stylesBackend, resources, images, imagesSvg, svgSprites);
