411707952b3a7183f7762e8a43ec134a09aa9dcc
saslavik <saslavik@gmail.com>
2021-06-04 18:18:15 +0300

    first commit

--- dashboard/src/components/Main.vue
+++ dashboard/src/components/Main.vue
@@ -3,11 +3,53 @@
       <v-tabs
         center-active
         dark
+        v-model="tab"
       >
-        <v-tab>Active auctions</v-tab>
-        <v-tab>Closed auctions</v-tab>
-        <v-tab>Listings</v-tab>
+        <v-tab v-for="item in items"
+          :key="item" >
+            {{ item }}
+        </v-tab>
       </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item"
+        >
+          <v-card flat>
+            <v-card-text>
+              <v-data-table
+                :headers="headers"
+                :items="desserts"
+                item-key="name"
+                class="elevation-1"
+                :search="search"
+                :custom-filter="filterOnlyCapsText"
+              >
+                <template v-slot:top>
+                  <v-text-field
+                    v-model="search"
+                    label="Search (UPPER CASE ONLY)"
+                    class="mx-4"
+                  ></v-text-field>
+                </template>
+                <template v-slot:body.append>
+                  <tr>
+                    <td></td>
+                    <td>
+                      <v-text-field
+                        v-model="calories"
+                        type="number"
+                        label="Less than"
+                      ></v-text-field>
+                    </td>
+                    <td colspan="4"></td>
+                  </tr>
+                </template>
+              </v-data-table>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
     </v-card>
 </template>
 
@@ -19,10 +61,127 @@
       return {
         tab: null,
         items: [
-          'web', 'shopping', 'videos', 'images', 'news',
+          'Active auctions', 'Closed auctions', 'Listings'
         ],
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        search: '',
+        calories: '',
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
       }
+    },
+    computed: {
+      headers () {
+        return [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          {
+            text: 'Calories',
+            value: 'calories',
+            filter: value => {
+              if (!this.calories) return true
+
+              return value < parseInt(this.calories)
+            },
+          },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ]
+      },
+    },
+    methods: {
+      filterOnlyCapsText (value, search, item) {
+        return value != null &&
+          search != null &&
+          typeof value === 'string' &&
+          value.toString().toLocaleUpperCase().indexOf(search) !== -1
+      },
     },
   }
 </script>
