7d97925f54f6c7f3245a156b489f981503eb5e68
Slavik <saslavik@gmail.com>
2021-06-04 11:13:29 +0300

    first commit

---
+++ dashboard/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      dark
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app dark dense>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
---
+++ dashboard/src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card>
+      <v-tabs
+        center-active
+        dark
+      >
+        <v-tab>Active auctions</v-tab>
+        <v-tab>Closed auctions</v-tab>
+        <v-tab>Listings</v-tab>
+      </v-tabs>
+    </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Main',
+
+    data () {
+      return {
+        tab: null,
+        items: [
+          'web', 'shopping', 'videos', 'images', 'news',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    },
+  }
+</script>
